f4598e996451ea8db0bd091a9a584be2ed9fe7e0
Victor Badenas <victor.badenas@gmail.com>
2021-05-18 12:52:56 +0200

    added sklearn

--- src/artificial_dataset_generator.py
+++ src/artificial_dataset_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn import datasets
 
 class ArtificialDatasetGenerator:
     def __init__(self, K, d, n):
@@ -9,9 +10,15 @@ class ArtificialDatasetGenerator:
     def __call__(self):
         return self._generate_dataset()
 
+    def __iter__(self):
+        return self
+
+    def __getitem__(self, *args, **kwargs):
+        return self._generate_dataset()
+
     @property
     def shape(self):
-        return (self.n, self.d)
+        return ((self.n, self.d), (self.n,))
 
     def __str__(self):
         return str(self.__dict__())
@@ -20,4 +27,4 @@ class ArtificialDatasetGenerator:
         return self.__str__()
 
     def _generate_dataset(self):
-        pass
+        return datasets.make_blobs(n_samples=self.n, n_features=self.d, centers=self.K)
