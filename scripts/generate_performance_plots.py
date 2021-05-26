015551f30119687ec034a0a5cdfd5ef9ce56ccca
Victor Badenas <victor.badenas@gmail.com>
2021-05-26 23:52:45 +0200

    add map to string and output name

--- scripts/generate_performance_plots.py
+++ scripts/generate_performance_plots.py
@@ -13,6 +13,7 @@ def parse_args():
     parser = argparse.ArgumentParser('Script to generate plots from a results csv')
     parser.add_argument('-i', '--csvPath', type=Path, help='path to csv file', default='results/artificial.csv')
     parser.add_argument('-t', '--targetvar', type=str, help='csv variable to plot', default='distance_calculations_average')
+    parser.add_argument('-n', '--plotName', type=Path, help='path to store the plot', required=True)
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -44,7 +45,7 @@ if __name__ == "__main__":
                     ax_data_ = ax_data_[ax_data_[k] == v]
                 x = ax_data_[x_name]
                 y = ax_data_[var_name]
-                axes[i][j].loglog(x, y, label=":".join(model.values()), marker='x', color=colors[color_idx%len(colors)])
+                axes[i][j].loglog(x, y, label=":".join(map(str, model.values())), marker='x', color=colors[color_idx%len(colors)])
             axes[i][j].grid('on')
             # axes[i][j].legend()
 
@@ -58,4 +59,4 @@ if __name__ == "__main__":
     f.legend(handles, labels)
     f.suptitle(f'{var_name} vs {x_name}', fontsize=16)
     # plt.tight_layout()
-    plt.savefig(f'images/{var_name}.png', dpi=400)
+    plt.savefig(args.plotName, dpi=400)
